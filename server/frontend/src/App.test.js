45ced68376bfe2a17a56f05d0941655ba326da9a
Zac Waite <129032699zw@gmail.com>
2021-09-28 00:25:54 -0400

    Compiled typescript on server

---
+++ server/frontend/src/App.test.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = __importDefault(require("react"));
+var react_2 = require("@testing-library/react");
+var App_1 = __importDefault(require("./App"));
+test('renders learn react link', function () {
+    (0, react_2.render)(react_1.default.createElement(App_1.default, null));
+    var linkElement = react_2.screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+});
