b575fe6d3686665c0c7a6277e9149f49b28038f1
Zac Waite <129032699zw@gmail.com>
2021-09-28 03:53:54 -0400

    quick bug fixes

--- server/backend/src/server.ts
+++ server/backend/src/server.ts
@@ -14,14 +14,14 @@ env.config();
 // utilities
 app.use(cors()); 
 app.use(express.json()); 
-app.use(express.static(path.resolve(__dirname, '../../Frontend/build')));
+app.use(express.static(path.resolve(__dirname, '../../frontend/build')));
 
 // routes
 import sensorsRoute from './route/sensors.route';
 import getFile from './route/files.route';
 
 // api
-app.use("/api/stores", sensorsRoute);
+app.use("/api/sensors", sensorsRoute);
 
 
 app.get("/files", getFile); 
@@ -32,7 +32,7 @@ app.get("/callback", (req, res) => {
 });
 
 app.get('*', (req: express.Request, res: express.Response) => {
-    res.sendFile(path.resolve(__dirname, '../../Frontend/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../../frontend/build', 'index.html'));
     //console.log(path.resolve(__dirname, '../../Frontend/build', 'index.html'));
 });
 
