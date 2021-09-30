9f635273769c8d1af2544e2d33627b582e07fad1
George Shao <GeorgeShao123@gmail.com>
2021-09-29 22:55:53 -0400

    Added and configured React Router

--- server/frontend/src/index.tsx
+++ server/frontend/src/index.tsx
@@ -4,9 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ChakraProvider } from "@chakra-ui/react";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ server/frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dashboard() {
+    return (
+        <div>
+            dashboard
+        </div>
+    )
+}
+
+export default Dashboard
