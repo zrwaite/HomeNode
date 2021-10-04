f0a2793fd4df47284426e84c240e978280b2d70a
George Shao <GeorgeShao123@gmail.com>
2021-10-03 20:39:51 -0400

    Updated Landing page to include hero component

--- server/frontend/src/pages/Landing.tsx
+++ server/frontend/src/pages/Landing.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
 
 function Landing() {
     return (
         <div>
             <Navbar />
+            <Hero />
         </div>
     )
 }
