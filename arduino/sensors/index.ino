4feb689356bbebc99b48aa6d90b72c050c7564f6
Hargun Mujral <hargunmujral@gmail.com>
2021-10-10 16:35:02 -0400

    storing random numbers into an array before processing

--- arduino/sensors/index.ino
+++ arduino/sensors/index.ino
@@ -1,12 +1,27 @@
 void setup() {
 Serial.begin(9600);
-
 randomSeed(analogRead(0));
+
+//The following is an arbitrary number of data points
+#define DATAPOINTS 10
+
+//We will enter each datapoint into an array, and for the sake of demonstration,
+//We will call a random number generator
+int storage[DATAPOINTS];
+  for (int i = 0; i < DATAPOINTS; i++){
+    storage[i] = generate(1);
+    Serial.println(storage[i]);
+  }
 
 }
 
 void loop() {
-  randNumber = random(100);
-  Serial.println(randNumber);
-  delay(50);
+
+}
+
+//random number generator
+int generate(int num){
+  int randNumber = random(100);
+  delay(100);
+  return randNumber;
 }
