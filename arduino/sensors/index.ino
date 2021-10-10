f272aa2060868513d8573eb9468b339017c7d4bb
Hargun Mujral <hargunmujral@gmail.com>
2021-10-10 17:18:17 -0400

    pseudocode written and average compution complete

--- arduino/sensors/index.ino
+++ arduino/sensors/index.ino
@@ -1,27 +1,55 @@
 void setup() {
-Serial.begin(9600);
-randomSeed(analogRead(0));
+  Serial.begin(9600);
+  randomSeed(analogRead(0));
 
-//The following is an arbitrary number of data points
-#define DATAPOINTS 10
+  //The following is an arbitrary number of data points
+  #define DATAPOINTS 10
 
-//We will enter each datapoint into an array, and for the sake of demonstration,
-//We will call a random number generator
-int storage[DATAPOINTS];
+  //the following is the tolerance to change before the temperature/humidity is updated
+  #define TOLERANCE 1
+
+  //We will enter each datapoint into an array, and for the sake of demonstration,
+  //We will call a random number generator
+  float storage[DATAPOINTS];
+  float counter = 0;
+
   for (int i = 0; i < DATAPOINTS; i++){
     storage[i] = generate(1);
     Serial.println(storage[i]);
+    counter += storage[i];
   }
+
+  float average = counter / DATAPOINTS;
+  Serial.println("AVERAGE:");
+  Serial.println(average);
 
 }
 
 void loop() {
+//use the sensor every 6 seconds and store temperature / humidity as a datapoint
+//once there are 10 datapoints, compute average
 
+//We must first include in the loop to check the last 5 displayed temperatures and
+//and whether its displayAverage5 is within TOLERANCE of the average. If so, keep the displayed
+//temperature the same and wipe the data points
+
+//else{
+
+  //if abs(average - oldaverage) >= TOLERANCE
+    // oldAverage = average
+    // return oldAverage to the display
+
+  //else
+    //for (int i = 0; i < DATAPOINTS; i++){
+      //storage[i] = 0;
+    //}
+  // We will keep the displayed temperature the same while it isn't updated
+//}
 }
 
 //random number generator
-int generate(int num){
-  int randNumber = random(100);
+float generate(int num){
+  float randNumber = random(100);
   delay(100);
-  return randNumber;
+  return randNumber / 15.00 + 200.00 / 8.00;
 }
