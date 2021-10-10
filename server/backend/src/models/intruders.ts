2b3ab233d3e38002ed1de1a2f6be092ce81e960a
Zac Waite <129032699zw@gmail.com>
2021-10-10 19:55:45 -0400

    Intruders mongoose schema

---
+++ server/backend/src/models/intruders.ts
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+//user schema that defines the entity
+const intrudersUpdateSchema = new mongoose.Schema({
+    detection : {
+        type: String,
+        required: false,
+    },
+    alert_level : {
+        type: Number,
+        required: false,
+    },
+}, {timestamps: true});
+
+const intrudersDailySchema = new mongoose.Schema({
+    date: {
+        type: String,
+        required: true,
+    },
+    intrusion_detections : {
+        type: Boolean,
+        required: false,
+    },
+}, {timestamps: true});
+
+const intrudersSchema = new mongoose.Schema({
+    // define type, required (the most important)
+    name: {
+        type: String,
+        required: true,
+    },
+    home_id: {
+        type: String,
+        required: true,
+    },  
+    current_data: {
+        type: intrudersUpdateSchema,
+        required: false,
+        default: {},
+    },
+    daily_data: {
+        type: [intrudersUpdateSchema],
+        required: false,
+        default: [],
+    },
+    past_data: {
+        type: [intrudersDailySchema],
+        required: false,
+        default: [],
+    },
+}, { timestamps: true});
+
+export default mongoose.model('Intruders', intrudersSchema); //Export data formatting
