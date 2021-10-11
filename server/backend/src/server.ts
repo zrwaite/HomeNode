c15a460ff08429c1c873ba266fa483c538892744
Zac Waite <129032699zw@gmail.com>
2021-10-11 18:33:09 -0400

    Home route complete! Lets goooooo

--- server/backend/src/server.ts
+++ server/backend/src/server.ts
@@ -21,13 +21,13 @@ app.use(express.static(path.resolve(__dirname, '../'))); //production
 import getFile from './route/files.route';
 import sensorsRoute from './route/sensors.route';
 import intrudersRoute from './route/intruders.route';
-// import homeRoute from './route/home.route';
+import homeRoute from './route/home.route';
 // import userRoute from './route/user.route';
 
 // api
 app.use("/api/sensors", sensorsRoute);
 app.use("/api/intruders", intrudersRoute);
-// app.use("/api/home", homeRoute);
+app.use("/api/home", homeRoute);
 // app.use("/api/user", userRoute);
 
 app.get("/files", getFile); 
