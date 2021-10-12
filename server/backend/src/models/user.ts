77aca435e71e511305eb460afd5af86fafe3a01e
Zac Waite <129032699zw@gmail.com>
2021-10-12 11:01:01 -0400

    Created models

---
+++ server/backend/src/models/user.ts
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+//user schema that defines the entity
+const userSettingsSchema = new mongoose.Schema({
+    dark_mode : {
+        type: Boolean,
+        required: true,
+    },
+    email_notifications : {
+        type: Boolean,
+        required: true,
+    }
+}, {timestamps: true});
+
+const userSchema = new mongoose.Schema({
+    // define type, required (the most important)
+    username: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    home_id: {
+        type: mongoose.Types.ObjectId,
+        required: true,  
+    },
+    settings: {
+        type: userSettingsSchema,
+        required: false,
+        default: {},
+    },
+}, { timestamps: true});
+
+export default mongoose.model('User', userSchema); //Export data formatting
