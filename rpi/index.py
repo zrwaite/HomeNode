2cc5515948619d9341f434ea05737b112817464e
Gongsta <gong.steven@hotmail.com>
2021-10-23 22:47:19 -0400

    fix some stupid bugs, still need to fix

--- rpi/index.py
+++ rpi/index.py
@@ -1,32 +1,52 @@
 import requests
+import simplejson
 import json
+from serial import Serial
+import time
 
-SERVER_ADDRESS = "159.89.119.204"
-GET_DIRECTORY = "/get" #TODO: Discuss with Zac
+SERVER_ADDRESS = "http://homenode.tech/api"
 
-HOME_URL = "/api/home"
-SENSORS_URL = "/api/sensors"
-INTRUDERS_URL = "/api/intruders"
-USER_URL = "/api/user"
+HOME_URL = "/home"
+SENSORS_URL = "/sensors"
+INTRUDERS_URL = "/intruders"
+USER_URL = "/user"
 
+
+# Not really needed since the requests library accepts python dictionaries
 def parse_to_json(python_object):
-  return json.dumps(python_object) 
+  return json.dumps(python_object, separators=(',', ':')) 
 
 def unparse_to_json(json_string):
   return json.loads(json_data)
 
-def post_data(json_data):
-  response = requests.post(SERVER_ADDRESS, data=json_data)
+def test_post_data(json_data):
+  headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+  response = requests.post('https://httpbin.org/post', headers=headers, data=json_data)
+  return response
+  
+def test_put_data(json_data):
+  headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+  response = requests.put('https://httpbin.org/put', headers=headers, data=json_data)
+  return response
+  
+def post_data(json_data, sub_url):
+  headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+  response = requests.post(SERVER_ADDRESS + sub_url, headers=headers, data=json_data)
   return response
 
-def get_data(headers={}, payload={}):
-  r = requests.get(SERVER_ADDRESS + SUB_URL, headers=headers, params=payload)
+
+def get_data(sub_url, headers={}, payload={}):
+  response = requests.get(SERVER_ADDRESS + sub_url, headers=headers, params=payload)
+  return response
 
 def put_data(json_data, sub_url):
   response = requests.put(SERVER_ADDRESS + sub_url, data=json_data)
   return response
 
-def push_home_data(json_data):
+def post_home_data(json_data):
   """
   {
 	"name": "Example Home",
@@ -35,7 +55,8 @@ def push_home_data(json_data):
 	"notifications": []
 }	
   """
-  push_data(json_data, HOME_URL)
+  response = post_data(json_data, HOME_URL)
+  return response
 
 
 def put_home_data(json_data):
@@ -56,10 +77,12 @@ def put_home_data(json_data):
 	}	
 }	
   """
-  put_data(json_data, HOME_URL)
+  response = put_data(json_data, HOME_URL)
+  return response
+
   
 
-def push_sensors_data(json_data):
+def post_sensors_data(json_data):
   """
 {
 	"name": "Barry's Sensors",
@@ -72,7 +95,8 @@ def push_sensors_data(json_data):
 	}
 }	
   """
-  push_data(json_data, SENSORS_URL)
+  response = post_data(json_data, SENSORS_URL)
+  return response
 
 def put_sensors_data(json_data):
   """Sample JSON 
@@ -83,10 +107,11 @@ def put_sensors_data(json_data):
     "light_level": 42
 }
 """
-  put_data(json_data, SENSORS_URL)
+  response = put_data(json_data, SENSORS_URL)
+  return response
   
 
-def push_intruders_data(json_data):
+def post_intruders_data(json_data):
   """
   {
 	"name": "Barry's Intrusion detection",
@@ -99,7 +124,8 @@ def push_intruders_data(json_data):
 	}
 }	
   """
-  push_data(json_data, INTRUDERS_URL)
+  response = post_data(json_data, INTRUDERS_URL)
+  return response
 
 def put_intruders_data(json_data):
   """Sample JSON 
@@ -108,17 +134,18 @@ def put_intruders_data(json_data):
 	"alert_level": 2
 }
 """
-  put_data(json_data, INTRUDERS_URL)
+  response = put_data(json_data, INTRUDERS_URL)
+  return response
   
-
-def push_user_data(json_data):
+def post_user_data(json_data):
   """
 {
 	"username": "barryhawkener@gmail.com",
 	"name": "Barry"
 }	
   """
-  push_data(json_data, USER_URL)
+  response = post_data(json_data, USER_URL)
+  return response
 
 def put_user_data(json_data):
   """
@@ -136,4 +163,49 @@ def put_user_data(json_data):
 	}
 }
 """
-  put_data(json_data, USER_URL)
+  response = put_data(json_data, USER_URL)
+  return response
+
+test = {
+    "id": "616b7f4a3a200197bf2207ee",
+    "light_level": 41,
+    "temperature": 21,
+    "humidity": 41
+}
+
+
+# This doesn't work
+response = put_sensors_data(parse_to_json(test))
+print(response.text)
+
+
+'''This WORKS, it still has the backslashes...
+test = {'name': 'Zach'}
+response = test_post_data(parse_to_json(test))
+print(response.text)
+response = post_home_data(parse_to_json(test))
+print(response.text)
+'''
+
+# while True:
+#     # Open Serial
+#     ser = Serial(port='/dev/ttyS0', baudrate=9600)  # open serial port
+#     ser.write(str.encode('1'))
+
+#     time.sleep(1)
+
+#     s = ser.readline()    
+#     data = s.decode('UTF-8')
+    
+#     data_list = data.split('/')
+    
+#     data_dict = {}
+#     for index, value in enumerate(data_list):
+#         if index % 2 == 0:
+#             data_dict[data_list[index]] = float(data_list[index+1].replace('\\','').strip())
+        
+#     print(data_dict)
+    
+#     data_dict['id'] = "616b7f4a3a200197bf2207ee"
+#     r = put_sensors_data((data_dict))
+#     print(r.text)
