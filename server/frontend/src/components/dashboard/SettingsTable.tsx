a6c3513fac9ed34b83304a4ac53b8f68e6af8672
George Shao <GeorgeShao123@gmail.com>
2021-10-26 16:49:24 -0400

    Updated settings table styling and added dark mode toggle

--- server/frontend/src/components/dashboard/SettingsTable.tsx
+++ server/frontend/src/components/dashboard/SettingsTable.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import {
   Flex,
   useColorModeValue,
@@ -10,11 +10,16 @@ import {
   Th,
   Td,
   Tbody,
+  Switch,
+  useColorMode,
 } from "@chakra-ui/react";
 import { AiFillEdit } from "react-icons/ai";
 import { BsBoxArrowUpRight, BsFillTrashFill } from "react-icons/bs";
+import UserContext from "../../User";
 
 export default function SettingsTable() {
+  const user = useContext(UserContext);
+  const { colorMode, toggleColorMode } = useColorMode();
   const header = ["key", "value", "actions"];
   const data = [
     { key: "Dark Mode", value: "OFF" },
@@ -23,160 +28,147 @@ export default function SettingsTable() {
     { key: "Setting #4", value: "N/A" },
   ];
   return (
-    <Flex
+    <Table
       w="full"
-      bg="gray.600"
-      p={50}
-      alignItems="center"
-      justifyContent="center"
+      bg={useColorModeValue("white", "gray.800")}
+      display={{
+        base: "block",
+        md: "table",
+      }}
+      sx={{
+        "@media print": {
+          display: "table",
+        },
+      }}
     >
-      <Table
-        w="full"
-        bg={useColorModeValue("white", "gray.800")}
+      <Thead
         display={{
-          base: "block",
-          md: "table",
+          base: "none",
+          md: "table-header-group",
         }}
         sx={{
           "@media print": {
-            display: "table",
+            display: "table-header-group",
           },
         }}
       >
-        <Thead
-          display={{
-            base: "none",
-            md: "table-header-group",
-          }}
-          sx={{
-            "@media print": {
-              display: "table-header-group",
-            },
-          }}
-        >
-          <Tr>
-            {header.map((x) => (
-              <Th key={x}>{x}</Th>
-            ))}
-          </Tr>
-        </Thead>
-        <Tbody
-          display={{
-            base: "block",
-            lg: "table-row-group",
-          }}
-          sx={{
-            "@media print": {
-              display: "table-row-group",
-            },
-          }}
-        >
-          {data.map((token, tid) => {
-            return (
-              <Tr
-                key={tid}
+        <Tr>
+          {header.map((x) => (
+            <Th key={x}>{x}</Th>
+          ))}
+        </Tr>
+      </Thead>
+      <Tbody
+        display={{
+          base: "block",
+          lg: "table-row-group",
+        }}
+        sx={{
+          "@media print": {
+            display: "table-row-group",
+          },
+        }}
+      >
+        {data.map((token, tid) => {
+          return (
+            <Tr
+              key={tid}
+              display={{
+                base: "grid",
+                md: "table-row",
+              }}
+              sx={{
+                "@media print": {
+                  display: "table-row",
+                },
+                gridTemplateColumns: "minmax(0px, 35%) minmax(0px, 65%)",
+                gridGap: "10px",
+              }}
+            >
+              {Object.keys(token).map((x) => {
+                return (
+                  <React.Fragment key={`${tid}${x}`}>
+                    <Td
+                      display={{
+                        base: "table-cell",
+                        md: "none",
+                      }}
+                      sx={{
+                        "@media print": {
+                          display: "none",
+                        },
+                        textTransform: "uppercase",
+                        color: "gray.400",
+                        fontSize: "xs",
+                        fontWeight: "bold",
+                        letterSpacing: "wider",
+                        fontFamily: "heading",
+                      }}
+                    ></Td>
+                    <Td color="gray.500" fontSize="md">
+                      {x === "key" ? data[tid]["key"] : data[tid]["value"]}
+                    </Td>
+                  </React.Fragment>
+                );
+              })}
+              <Td
                 display={{
-                  base: "grid",
-                  md: "table-row",
+                  base: "table-cell",
+                  md: "none",
                 }}
                 sx={{
                   "@media print": {
-                    display: "table-row",
+                    display: "none",
                   },
-                  gridTemplateColumns: "minmax(0px, 35%) minmax(0px, 65%)",
-                  gridGap: "10px",
+                  textTransform: "uppercase",
+                  color: "gray.400",
+                  fontSize: "xs",
+                  fontWeight: "bold",
+                  letterSpacing: "wider",
+                  fontFamily: "heading",
                 }}
               >
-                {Object.keys(token).map((x) => {
-                  return (
-                    <React.Fragment key={`${tid}${x}`}>
-                      <Td
-                        display={{
-                          base: "table-cell",
-                          md: "none",
-                        }}
-                        sx={{
-                          "@media print": {
-                            display: "none",
-                          },
-                          textTransform: "uppercase",
-                          color: "gray.400",
-                          fontSize: "xs",
-                          fontWeight: "bold",
-                          letterSpacing: "wider",
-                          fontFamily: "heading",
-                        }}
-                      >
-                      </Td>
-                      <Td color="gray.500" fontSize="md" fontWeight="hairline">
-                        {x === "key" ? data[tid]['key'] : data[tid]['value']}
-                      </Td>
-                    </React.Fragment>
-                  );
-                })}
-                <Td
-                  display={{
-                    base: "table-cell",
-                    md: "none",
-                  }}
-                  sx={{
-                    "@media print": {
-                      display: "none",
-                    },
-                    textTransform: "uppercase",
-                    color: "gray.400",
-                    fontSize: "xs",
-                    fontWeight: "bold",
-                    letterSpacing: "wider",
-                    fontFamily: "heading",
-                  }}
-                >
-                  Actions
-                </Td>
-                <Td>
-                  {data[tid]['key'] === "Dark Mode" ? (
-                    <ButtonGroup variant="solid" size="sm" spacing={3}>
-                      <IconButton
-                        colorScheme="green"
-                        icon={<AiFillEdit />}
-                        aria-label="button"
-                      />
-                    </ButtonGroup>
-                  ) : (
-                    <ButtonGroup variant="solid" size="sm" spacing={3}>
-                      <IconButton
-                        colorScheme="green"
-                        icon={<AiFillEdit />}
-                        aria-label="button"
-                      />
-                    </ButtonGroup>
-                  )}
-                </Td>
-                {/* <Td>
+                Actions
+              </Td>
+              <Td>
+                {data[tid]["key"] === "Dark Mode" ? (
                   <ButtonGroup variant="solid" size="sm" spacing={3}>
-                    <IconButton
-                      colorScheme="blue"
-                      icon={<BsBoxArrowUpRight />}
-                      aria-label="button"
-                    />
+                    <Switch size="lg" onChange={toggleColorMode} />
+                  </ButtonGroup>
+                ) : (
+                  <ButtonGroup variant="solid" size="sm" spacing={3}>
                     <IconButton
                       colorScheme="green"
                       icon={<AiFillEdit />}
                       aria-label="button"
                     />
-                    <IconButton
-                      colorScheme="red"
-                      variant="outline"
-                      icon={<BsFillTrashFill />}
-                      aria-label="button"
-                    />
                   </ButtonGroup>
-                </Td> */}
-              </Tr>
-            );
-          })}
-        </Tbody>
-      </Table>
-    </Flex>
+                )}
+              </Td>
+              {/* <Td>
+                <ButtonGroup variant="solid" size="sm" spacing={3}>
+                  <IconButton
+                    colorScheme="blue"
+                    icon={<BsBoxArrowUpRight />}
+                    aria-label="button"
+                  />
+                  <IconButton
+                    colorScheme="green"
+                    icon={<AiFillEdit />}
+                    aria-label="button"
+                  />
+                  <IconButton
+                    colorScheme="red"
+                    variant="outline"
+                    icon={<BsFillTrashFill />}
+                    aria-label="button"
+                  />
+                </ButtonGroup>
+              </Td> */}
+            </Tr>
+          );
+        })}
+      </Tbody>
+    </Table>
   );
 }
