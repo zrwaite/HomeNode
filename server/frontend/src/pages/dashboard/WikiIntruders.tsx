c12a3d842df65de7fa3cc14ecdd6dfd0c22e0af2
George Shao <GeorgeShao123@gmail.com>
2021-11-08 21:38:22 -0500

    Removed markdown display testing content

--- server/frontend/src/pages/dashboard/WikiIntruders.tsx
+++ server/frontend/src/pages/dashboard/WikiIntruders.tsx
@@ -1,19 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ReactMarkdown from "../../components/dashboard/Markdown";
 
-let content = `
-A paragraph with *emphasis* and **strong importance**.
-
-> A block quote with ~strikethrough~ and a URL: https://reactjs.org.
-
-* Lists
-* [ ] todo
-* [x] done
-
-A table:
-
-| a | b |
-| - | - |`;
+let content = ``;
 
 function WikiIntruders() {
   return <ReactMarkdown content={content} />;
