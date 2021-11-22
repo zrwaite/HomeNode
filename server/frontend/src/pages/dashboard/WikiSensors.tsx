f8803f4e847e75ebc7546f1011d831388d24c5d8
George Shao <GeorgeShao123@gmail.com>
2021-11-22 03:42:36 -0500

    Updated Wiki templates

--- server/frontend/src/pages/dashboard/WikiSensors.tsx
+++ server/frontend/src/pages/dashboard/WikiSensors.tsx
@@ -1,10 +1,17 @@
-import React, { useState, useEffect } from "react";
-import ReactMarkdown from "../../components/dashboard/Markdown";
-
-let content = ``;
+import React from "react";
+import { Flex, Heading, Text } from "@chakra-ui/react";
 
 function WikiSensors() {
-  return <ReactMarkdown content={content} />;
+  return (
+    <Flex alignItems="start" flexDirection="column" gap="500px">
+      <Heading size="2xl">Sensors Module</Heading>
+      <Heading size="xl">Summary</Heading>
+      <Text>
+        Sensors are the main way to get information about the state of the
+        system. They are used to get information about the state of the system.
+      </Text>
+    </Flex>
+  );
 }
 
 export default WikiSensors;
