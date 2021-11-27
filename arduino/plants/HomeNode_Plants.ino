2e46516dc985720f0e48aed687650ec8b3cd498c
MrBruh <mdnssv@gmail.com>
2021-11-27 12:35:43 -0500

    Add control of the light for the plants module

--- arduino/plants/HomeNode_Plants.ino
+++ arduino/plants/HomeNode_Plants.ino
@@ -1,10 +1,16 @@
 // Hardcoded parameters
-#define MEASURING_DATAPOINTS 5  // The amount of datapoints we take for a measurement
-#define COMPARING_DATAPOINTS 5 // The amount of measurements we use to compare data for significance
-#define MEASURING_INTERVAL 200  // Milliseconds between individual sensor readings
+#define MEASURING_DATAPOINTS 3  // The amount of datapoints we take for a measurement
+#define COMPARING_DATAPOINTS 3  // The amount of measurements we use to compare data for significance
+#define MEASURING_INTERVAL 1100  // Milliseconds between individual sensor readings
 #define TOLERANCE 1.0           // Change in data before we deem in significant
 #define ADDRESS '2'             // Address of the module
 #define SENSORS_NUM 2           // Number of sensors on the module
+#define LIGHT_THRESHOLD 15      // At what level of light will the module turn on it's own light
+#define WATER_THRESHOLD 40      // At what level of moisture will the module water the plant
+#define WATERING_DURATION 2000  // Number of milliseconds the module will be watering the plant
+#define WATERING_INTERVAL 30    // Number of seconds before the module can water the plant again
+#define LIGHT_PIN 3
+#define WATERING_PIN 2
 
 // Global data storage variables:
 
@@ -19,8 +25,8 @@ typedef struct {
   float measurements[COMPARING_DATAPOINTS]; // Past measurements
 } Sensor;
 
-Sensor Light_sensor = {A0, "plant_light_level", 0, 0, 0};
-Sensor Moisture_sensor = {A1, "plant_moisture", 0, 0, 0};
+Sensor Light_sensor = {A1, "plant_light_level", 0, 0, 0};
+Sensor Moisture_sensor = {A2, "plant_moisture", 0, 0, 0};
 
 Sensor *sensors[] = {&Light_sensor, &Moisture_sensor};
 
@@ -28,6 +34,8 @@ int data_counter = 0;       // Counter for amount of datapoints read
 int measurement_counter = 0;// Counter for amount of measurements measured
 float timer = 0;            // Timer used for measruing interval
 char address = ADDRESS;     // Address of the module
+
+int light_state = 0;
 
 void init_array(float arr[], int len){
   for(int i = 0; i < len; ++i){
@@ -48,6 +56,10 @@ void setup() {
 
   // Initialize sensor pins
   // However no initialization is necessary since analog pins are only input
+
+  // Non-sensor pins:
+  pinMode(LIGHT_PIN, OUTPUT);
+  pinMode(WATERING_PIN, OUTPUT);
 }
 
 // Raw data from sensors are on a scale of 0-1024, which we convert to 0-100
@@ -55,14 +67,32 @@ float convertData(float data){
   return (float)map(data, 1023, 0, 0, 100);
 }
 
+float convertDataLight(float data){
+  return (float)map(data, 0, 1023, 0, 100);
+}
+
 // Functions for reading sensor data
 float readLight() {
-  return convertData(analogRead(Light_sensor.pin));
+  return convertDataLight(analogRead(Light_sensor.pin));
 }
 
 float readMoisture() {
   return convertData((analogRead(Moisture_sensor.pin) +
                       analogRead(Moisture_sensor.pin + 1) ) / 2);
+}
+
+// Functions for taking care of the plant
+void checkLight(float light){
+  if(light <= LIGHT_THRESHOLD && light_state == 0) {
+    digitalWrite(LIGHT_PIN, 1);
+    Serial.print("plants_light_switch/1\\\n\r");
+    light_state = 1;
+    
+  } else if(light_state == 1) {
+    digitalWrite(LIGHT_PIN, 0);
+    Serial.print("plants_light_switch/0\\\n\r");
+    light_state = 0;
+  }
 }
 
 void loop() {
@@ -122,6 +152,11 @@ void loop() {
         if(sensors[i]->significant_flag == 1){
           sensors[i]->significant_flag = 0;
           sensors[i]->active_data = sensors[i]->measurements[current_measurement];
+        }
+
+        // Checking if any actions need to be performed
+        if(sensors[i]->tag == "plant_light_level"){
+          checkLight(sensors[i]->measurements[current_measurement]);
         }
       }
     }
