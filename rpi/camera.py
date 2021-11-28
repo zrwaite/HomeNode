253ebf421053c5178c60da0b8bae0545fce1c703
Gongsta <gong.steven@hotmail.com>
2021-11-27 20:33:13 -0500

    fix camera

--- rpi/camera.py
+++ rpi/camera.py
@@ -1,6 +1,8 @@
 from picamera import PiCamera
 
 def capture_image_from_rpi_camera():
+    if not os.path.isdir('./images'):
+        os.mkdir('./images')
     camera = PiCamera()
     camera.capture('./images/picture.jpg')
 
